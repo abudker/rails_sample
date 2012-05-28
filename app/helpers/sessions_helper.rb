15948ef5a216c69d5399f3b08868aa3c9cd0f760
Andrew Budker <abudker@gmail.com>
2012-05-28 14:02:21 -0700

    Finish sign in

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,25 @@
+module SessionsHelper
+
+  def current_user=(user)
+    @current_user = user
+  end
+
+  def current_user
+    @current_user ||= User.find_by_remember_token(cookies[:remember_token])
+  end
+
+  def signed_in?
+    !current_user.nil?
+  end
+  
+  def sign_in(user)
+    cookies[:remember_token] = { value:   user.remember_token,
+                                expires: 1.hour.from_now.utc }
+    current_user = user
+  end
+
+  def sign_out
+    current_user = nil
+    cookies.delete(:remember_token)
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -22,6 +22,7 @@ describe User do
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
 
@@ -40,6 +41,11 @@ describe User do
   describe "when name is too long" do
     before { @user.name = "a" * 51 }
     it { should_not be_valid }
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
   end
 
 end
