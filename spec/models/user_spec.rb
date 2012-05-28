3919901d719f52e71ddcaa67ae53cff4f0c449c1
Andrew Budker <abudker@gmail.com>
2012-05-28 12:38:26 -0700

    Implement User Sign Up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,13 +14,16 @@ require 'spec_helper'
 describe User do
 
   before do
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "foobar", password_confirmation: "foobar")
   end
 
   subject { @user }
 
   it { should respond_to(:name) }
   it { should respond_to(:email) }
+  it { should respond_to(:password_digest) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
 
   it { should be_valid }
 
