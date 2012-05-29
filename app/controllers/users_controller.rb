c44591026d9e7cb30639336de6a0bb384dff3aac
Andrew Budker <abudker@gmail.com>
2012-05-28 23:25:02 -0700

    Add authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:edit, :index,  :update]
+  before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
+  before_filter :unsigned_in_user,     only: [:create, :new]
+
   def new
     @user = User.new
   end
@@ -18,7 +23,48 @@ class UsersController < ApplicationController
     end
   end
 
-  def index
-    @users = User.all
+  def update
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile updated"
+      sign_in @user
+      redirect_to @users
+    else
+      render 'edit'
+    end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_path
+  end
+
+  def edit
+  end
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+  
+  private
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_path, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end
+
+    def unsigned_in_user
+      redirect_to(root_path) unless !signed_in?
+    end
+
 end
